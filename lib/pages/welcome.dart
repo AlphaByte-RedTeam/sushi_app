41780169a7bead830e17b3a87fbdc3a95bb992cd
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-09 19:14:42 +0700

    Update import statement for login screen

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:sushi_app/pages/login.dart';
+import 'package:sushi_app/ui/screens/login.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -27,6 +27,7 @@ class Welcome extends StatelessWidget {
                   ),
                 ),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     SvgPicture.asset(
                       "assets/images/welcome.svg",
