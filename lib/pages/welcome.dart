e45672afe1233292d8fab1033108ccb2669dd56b
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-09 17:53:21 +0700

    add welcome screen

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -6,9 +9,70 @@ class Welcome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          "Welcome to Sushi App",
+      backgroundColor: const Color(0xFFF06543),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Shu Shie",
+                  style: GoogleFonts.comfortaa(
+                    fontSize: 32,
+                    color: Colors.white,
+                  ),
+                ),
+                Column(
+                  children: [
+                    SvgPicture.asset(
+                      "assets/images/welcome.svg",
+                      height: 300,
+                    ),
+                    const Gap(16),
+                    Text(
+                      "The best sushi in town",
+                      style: GoogleFonts.comfortaa(
+                        fontSize: 48,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Gap(16),
+                    Text(
+                      "Feel the taste of the most popular Japanese food from anywhere and anytime",
+                      style: GoogleFonts.comfortaa(
+                        fontSize: 14,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                        ),
+                        icon: const Icon(Icons.arrow_forward),
+                        label: Text(
+                          "Get Started",
+                          style: GoogleFonts.comfortaa(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
