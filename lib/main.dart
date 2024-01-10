f35fd422f36371dcadbe67e4835dcaa8e4c80c52
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-10 12:34:23 +0700

    add specific routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sushi_app/pages/home.dart';
 import 'package:sushi_app/pages/welcome.dart';
+import 'package:sushi_app/ui/screens/login.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +22,10 @@ class MyApp extends StatelessWidget {
       home: const SafeArea(
         child: Welcome(),
       ),
+      routes: {
+        '/login': (context) => const Login(),
+        '/home': (context) => const Home(),
+      },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSwatch(
           primarySwatch: Colors.deepOrange,
