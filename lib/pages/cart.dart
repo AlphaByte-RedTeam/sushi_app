f4527189189c4d81b98e8c42d70e974e086c3221
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-13 13:41:52 +0700

    wip cart page

---
+++ lib/pages/cart.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Cart extends StatefulWidget {
+  const Cart({super.key});
+
+  @override
+  State<Cart> createState() => _CartState();
+}
+
+class _CartState extends State<Cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Cart',
+          style: GoogleFonts.comfortaa(color: Colors.white),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SafeArea(
+        child: const SingleChildScrollView(
+          child: Text('Cart'),
+        ),
+      ),
+    );
+  }
+}
