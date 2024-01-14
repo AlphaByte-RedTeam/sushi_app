21ec20091c747eb2c362a7f016984bec7b68ce26
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-14 23:40:15 +0700

    Add address creation functionality

--- lib/pages/address.dart
+++ lib/pages/address.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:radio_group_v2/radio_group_v2.dart';
 import 'package:sushi_app/helper/supabase_helper.dart';
 
 class Address extends StatefulWidget {
@@ -14,6 +15,12 @@ class Address extends StatefulWidget {
 
 class _AddressState extends State<Address> {
   final _stream = SupabaseHelper().fetchAndStreamTable('user_address');
+  TextEditingController labelController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController receiverController = TextEditingController();
+  TextEditingController phoneNumReceiverController = TextEditingController();
+  RadioGroupController isDefaultAddressController = RadioGroupController();
+  bool isDefaultAddress = false;
 
   @override
   Widget build(BuildContext context) {
@@ -72,7 +79,9 @@ class _AddressState extends State<Address> {
                                     icon: const Icon(
                                       Icons.edit,
                                     ),
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      // TODO: Continue the edit section
+                                    },
                                   )
                                 ],
                               ),
@@ -160,7 +169,168 @@ class _AddressState extends State<Address> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            useSafeArea: true,
+            showDragHandle: true,
+            builder: (context) => SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: labelController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        labelText: 'Label Alamat *',
+                        hintText: 'Contoh: Rumah, Kantor, dll',
+                      ),
+                    ),
+                    const Gap(16),
+                    TextFormField(
+                      controller: addressController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        labelText: 'Alamat *',
+                        hintText: 'Contoh: Jl. ABC Lima Dasar 123 no. 1',
+                      ),
+                    ),
+                    const Gap(16),
+                    TextFormField(
+                      controller: receiverController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        labelText: 'Nama Penerima *',
+                        hintText: 'Contoh: Jane Doe',
+                      ),
+                    ),
+                    const Gap(16),
+                    TextFormField(
+                      controller: phoneNumReceiverController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        labelText: 'No. Telp Penerima *',
+                        hintText: 'Contoh: 6287712345678',
+                      ),
+                    ),
+                    const Gap(16),
+                    const Text(
+                      'Jadikan Alamat Utama?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const Gap(16),
+                    RadioGroup(
+                      controller: isDefaultAddressController,
+                      values: const ["Yes", "No"],
+                      indexOfDefault: 0,
+                      orientation: RadioGroupOrientation.horizontal,
+                      onChanged: (value) => value == 'Yes'
+                          ? isDefaultAddress = true
+                          : isDefaultAddress = false,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () => Navigator.pop(context),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.black,
+                              side: const BorderSide(
+                                color: Colors.deepOrange,
+                              ),
+                            ),
+                            child: Text(
+                              'Cancel',
+                              style: GoogleFonts.comfortaa(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const Gap(16),
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              try {
+                                if (labelController.text.isNotEmpty &&
+                                    addressController.text.isNotEmpty &&
+                                    receiverController.text.isNotEmpty &&
+                                    phoneNumReceiverController
+                                        .text.isNotEmpty) {
+                                  SupabaseHelper().insertToTable(
+                                    'user_address',
+                                    {
+                                      'label_address': labelController.text,
+                                      'address': addressController.text,
+                                      'receiver_name': receiverController.text,
+                                      'receiver_phone_number':
+                                          phoneNumReceiverController.text,
+                                      'is_default_address': isDefaultAddress,
+                                    },
+                                  );
+                                  Navigator.pop(context);
+                                  labelController.clear();
+                                  addressController.clear();
+                                  receiverController.clear();
+                                  phoneNumReceiverController.clear();
+                                  log('insert success');
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                      backgroundColor: Colors.red,
+                                      content: Text(
+                                        'Please fill all the fields',
+                                        style: GoogleFonts.comfortaa(
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }
+                              } catch (e) {
+                                log(e.toString());
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.deepOrange,
+                              foregroundColor: Colors.white,
+                            ),
+                            child: Text(
+                              'Save Address',
+                              style: GoogleFonts.comfortaa(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
         backgroundColor: Colors.deepOrange,
         child: const Icon(Icons.add),
       ),
