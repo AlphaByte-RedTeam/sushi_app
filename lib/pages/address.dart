d02c8059607b9efb203ab4e8bdcf9c4c419eca42
Andrew Virya Victorio <andrew.avv03@gmail.com>
2024-01-14 22:46:33 +0700

    create listview address

--- lib/pages/address.dart
+++ lib/pages/address.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -52,7 +54,99 @@ class _AddressState extends State<Address> {
                       height: MediaQuery.of(context).size.height,
                       child: ListView.separated(
                         itemBuilder: (context, index) {
-                          // TODO: continue here
+                          return SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: ListTile(
+                              title: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    data[index]['label_address'].toUpperCase(),
+                                    style: GoogleFonts.comfortaa(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    icon: const Icon(
+                                      Icons.edit,
+                                    ),
+                                    onPressed: () {},
+                                  )
+                                ],
+                              ),
+                              leading: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.deepOrange,
+                                  shape: BoxShape.circle,
+                                  border: Border.all(color: Colors.black),
+                                ),
+                                padding: const EdgeInsets.all(4),
+                                child: const Icon(
+                                  Icons.home,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              isThreeLine: true,
+                              tileColor: Colors.black.withOpacity(0.05),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                                side: const BorderSide(
+                                  color: Colors.deepOrange,
+                                ),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                vertical: 8,
+                                horizontal: 16,
+                              ),
+                              subtitle: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '${data[index]['receiver_name']} - ${data[index]['receiver_phone_number']}',
+                                        style: GoogleFonts.comfortaa(
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.black.withOpacity(0.7),
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {
+                                          try {
+                                            SupabaseHelper().deleteFromTable(
+                                              'user_address',
+                                              data[index]['id'],
+                                            );
+                                          } catch (e) {
+                                            log(e.toString());
+                                          }
+                                        },
+                                        icon: const Icon(
+                                          Icons.delete,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    data[index]['address'].toUpperCase(),
+                                    style: GoogleFonts.comfortaa(
+                                      fontWeight: FontWeight.normal,
+                                      color: Colors.black.withOpacity(0.7),
+                                      fontSize: 12,
+                                    ),
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
                         },
                         separatorBuilder: (context, index) => const Gap(16),
                         itemCount: data.length,
